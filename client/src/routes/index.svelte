1ae7e58b8708f720e71e96c0bc1379d478668bfd
FlanB <clement.renou@mmibordeaux.com>
2022-01-16 23:54:36 +0100

    ✨ connect front-end and back-end

---
+++ client/src/routes/index.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { onMount } from "svelte"
+
+  let ads
+  onMount(() => {
+      fetch("http://localhost:3001")
+        .then(res => res.json())
+        .then(data => {
+          ads = data
+          console.log(ads)
+        })
+    }
+  )
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{#if ads}
+    <ul>
+        {#each ads as ad}
+            <li>
+                <h3>{ad._id}</h3>
+                <p>{ad.title}</p>
+            </li>
+            <hr>
+        {/each}
+    </ul>
+{/if}
