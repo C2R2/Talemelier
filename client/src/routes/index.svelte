1ac4d311852ae5d2ddebf3bca2f424ffe6728539
FlanB <clement.renou@mmibordeaux.com>
2022-01-25 17:11:33 +0100

    🔀 branch

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -9,24 +9,20 @@
         .then(res => res.json())
         .then(data => {
           ads = data
-          console.log(ads)
         })
     }
   )
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <form method="post" on:submit|preventDefault={
   () => {
-    console.log(title)
-
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:3001", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({
     title: title
-}))
+    }))
+    title = ""
   }
 }>
     <input bind:value={title} name="title" placeholder="title" type="text"/>
