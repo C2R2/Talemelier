1593549981ac36a0b5a90660a1d7168ae63d8b55
FlanB <clement.renou@mmibordeaux.com>
2022-02-26 14:16:18 +0100

    🔥 revert adapter vercel

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,10 +1,13 @@
-import vercel from "@sveltejs/adapter-vercel"
+import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import("@sveltejs/kit").Config} */
-export default {
-    kit: {
-        // hydrate the <div id="svelte"> element in src/app.html
-        target: "#svelte", adapter: vercel()
-    }
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
 
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;
