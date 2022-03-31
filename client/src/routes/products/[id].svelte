c04a4c8212e82a1fb6fcd3357244fd155aa41fe9
FlanB <clement.renou@mmibordeaux.com>
2022-04-01 01:16:27 +0200

    💄 quantity control style

--- client/src/routes/products/[id].svelte
+++ client/src/routes/products/[id].svelte
@@ -17,6 +17,19 @@
   <div class="infos">
     <span class="title">{productId}</span>
     <span class="price">Prix : <b>1,10 €</b></span>
+    <div class="quantity">
+      Quantité : 2
+      <div class="quantity-control">
+        <svg fill="none" height="10" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+             stroke-width="2" viewBox="5 0 15 10" width="15" xmlns="http://www.w3.org/2000/svg">
+          <polyline points="6 9 12 3 18 9"/>
+        </svg>
+        <svg fill="none" height="8" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+             stroke-width="2" viewBox="5 8 15 8" width="15" xmlns="http://www.w3.org/2000/svg">
+          <polyline points="6 9 12 15 18 9"/>
+        </svg>
+      </div>
+    </div>
     <span>Ingrédients :</span>
     <p class="ingredients">
       Farine, <b>oeuf</b>, beurre, <b>lait</b>, sel, levure, huile d'olive
@@ -66,6 +79,26 @@
     background-color: #D4CAC4;
     gap: 1rem;
   }
+
+  .quantity {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .quantity-control {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    >*{
+      cursor: pointer;
+    }
+  }
+
+  .ingredients {
+    color: #323232;
+  }
+
 
   .other {
     display: flex;
