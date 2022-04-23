026ca05e2a0d87a424e4cc8492c485f7b76c8701
FlanB <clement.renou@mmibordeaux.com>
2022-04-23 20:53:12 +0200

    ✨ user role

---
+++ client/src/routes/admin/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Cookies from "js-cookie"
+
+    let usersList
+
+    fetch("https://talemelier.herokuapp.com/users",
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + Cookies.get("token")
+            }
+        }
+    ).then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+
+</script>
+
+<br><br><br><br><br><br><br><br>
+<h1>Vous pouvez accéder à ce contenu</h1>
