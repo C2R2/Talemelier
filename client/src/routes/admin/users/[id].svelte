5105bee2b652d097ae3d72d4247dc7f156b58d92
FlanB <clement.renou@mmibordeaux.com>
2022-05-07 02:07:12 +0200

    ✨ prepare get single user promise

--- client/src/routes/admin/users/[id].svelte
+++ client/src/routes/admin/users/[id].svelte
@@ -1,8 +1,25 @@
 <script>
     import { page } from "$app/stores"
+    import Cookies from "js-cookie"
 
     const userID = $page.params.id
+
+    const userPromise = fetch("https://talemelier.herokuapp.com/users/" + userID, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + Cookies.get("token")
+        }
+    }).then(response => response.json())
 </script>
 
 
 <p>User: {userID}</p>
+
+{#await userPromise }
+  <p>Chargement...</p>
+{:then user}
+  <p>{user.email}</p>
+{:catch error}
+  <p>{error}</p>
+{/await}
