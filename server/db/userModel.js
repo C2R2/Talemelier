00ffa58a46d02acae63acae249376c9d63a81203
FlanB <clement.renou@mmibordeaux.com>
2022-05-16 22:37:01 +0200

    ✨ add image field

--- server/db/userModel.js
+++ server/db/userModel.js
@@ -4,15 +4,14 @@ const mongoose = require("mongoose")
 const UserSchema = new mongoose.Schema({
     // email field
     email: {
-        type: String, required: [true, "Please provide an Email!"], unique: [true, "Email Exist"]
+        type: String, required: [true, "Please provide an Email!"], unique: [true, "Email Exist"], trim: true, lowercase: true,
     },
-
     //   password field
     password: {
-        type: String, required: [true, "Please provide a password!"], unique: false
+        type: String, required: [true, "Please provide a password!"], unique: false, trim: true,
     },
     role: {
-        type: String, unique: false
+        type: String, unique: false, trim: true,
     },
 
 })
