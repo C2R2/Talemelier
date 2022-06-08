52df4b00e85623723f0e9912b300f33d14afde60
FlanB <clement.renou@mmibordeaux.com>
2022-06-08 19:00:48 +0200

    ✨ update user model

--- server/db/userModel.js
+++ server/db/userModel.js
@@ -1,22 +1,42 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 // user schema
 const UserSchema = new mongoose.Schema({
-    // email field
-    email: {
-        type: String,
-        required: [true, "Please provide an Email!"],
-        unique: [true, "Email Exist"],
-        trim: true,
-        lowercase: true
-    }, //   password field
-    password: {
-        type: String, required: [true, "Please provide a password!"], unique: false, trim: true
-    }, role: {
-        type: String, unique: false, trim: true
-    }
-
-})
+  // email field
+  email: {
+    type: String,
+    required: [true, "Please provide an Email!"],
+    unique: [true, "Email Exist"],
+    trim: true,
+    lowercase: true,
+  }, //   password field
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    unique: false,
+    trim: true,
+  },
+  role: {
+    type: String,
+    unique: false,
+    trim: true,
+  },
+  firstName: {
+    type: String,
+    unique: false,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    unique: false,
+    trim: true,
+  },
+  tel: {
+    type: Number,
+    unique: false,
+    trim: true,
+  },
+});
 
 // export UserSchema
-module.exports = mongoose.model("Users", UserSchema)
+module.exports = mongoose.model("Users", UserSchema);
